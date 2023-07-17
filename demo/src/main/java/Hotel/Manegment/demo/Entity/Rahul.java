5a2816c8d47548a694ec9e16dfe01f69968411b3
Rahul <rahulbodne2018@gmail.com>
2023-07-17 12:42:50 +0530

    2nd commit

---
+++ demo/src/main/java/Hotel/Manegment/demo/Entity/Rahul.java
@@ -0,0 +1,10 @@
+package Hotel.Manegment.demo.Entity;
+
+public class Rahul {
+
+    private  long id;
+    private  String name;
+    private  String fullname;
+    private  String finalname;
+    
+}
